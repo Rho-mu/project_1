00f49923d4dc0b04ed6c0405975bf255c8e457c3
rcm264 <rcm264@nau.edu>
2019-09-17 10:43:36 -0700

    Added arrow flight
    Arrows now fly accross the screen from right to left.

--- game.js
+++ game.js
@@ -1,15 +1,40 @@
 var gameport = document.getElementById("gameport");
 
+// Frame init
+var fps = 60;
 var WIDTH = 1000;
 var HEIGHT = 300;
-
 var renderer = PIXI.autoDetectRenderer({width: WIDTH, height: HEIGHT, backgroundColor: 0x1a52ff});
 gameport.appendChild(renderer.view);
 
 // Load assets
 var stage = new PIXI.Container();
-var arrow = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Sprites/Arrow.png"));
+var arrowTexture = PIXI.Texture.fromImage("Assets/Sprites/Arrow.png");
+//var arrow = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Sprites/Arrow.png"));
 var knight = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Sprites/Knight64.png"));
+
+// Score
+var score = 0;
+/*const scoreTextStyle = new Pixi.TextStyle(
+{
+  fontFamily: 'Arial',
+  fontSize: 36,
+  fontStyle: 'italic',
+  fontWeight: 'bold',
+  fill: ['#ffffff', '#00ff99'], // gradient
+  stroke: '#4a1850',
+  strokeThickness: 5,
+  dropShadow: true,
+  dropShadowColor: '#000000',
+  dropShadowBlur: 4,
+  dropShadowAngle: Math.PI / 6,
+  dropShadowDistance: 6
+});
+*/
+var scoreText = new PIXI.Text('score: ' + score);
+scoreText.x = 0;
+scoreText.y = 0;
+stage.addChild(scoreText);
 
 // Knight init
 knight.anchor.x = 0.5;
@@ -19,11 +44,16 @@ knight.position.y = HEIGHT-50;
 stage.addChild(knight);
 
 // Enemy init
-arrow.anchor.x = 0.5;
-arrow.anchor.y = 0.5;
-arrow.position.x = WIDTH;
-arrow.position.y = Math.floor(Math.random() * HEIGHT);
-stage.addChild(arrow);
+for(let i = 0; i < 10; i++)
+{
+  const arrow = new PIXI.Sprite(arrowTexture);
+  arrow.anchor.set(0.5);
+  arrow.x = WIDTH;
+  arrow.y = Math.floor(Math.random() * HEIGHT);
+  stage.addChild(arrow);
+  arrowFlight(arrow);
+}
+
 
 /*
 // Mouse functionality
@@ -56,6 +86,32 @@ function knightControlHander(e)
 }
 document.addEventListener('keydown', knightControlHander);
 
+
+// Enemy controls
+function arrowFlight(arrow)
+{
+  setTimeout(function()
+  {
+    if(arrow.position.y >= knight.position.y-32 &&
+       arrow.position.y <= knight.position.y+32 &&
+       arrow.position.x-8 <= knight.position.x+32)
+       {
+         score++;
+         stage.removeChild(arrow);
+         reutrn;
+       }
+
+    requestAnimationFrame(function(temp)
+    {
+      arrowFlight(arrow);
+    });
+
+    arrow.x -= 10;
+
+  }, 1000/ fps);
+}
+
+/*
 function enemyAI()
 {
   if(arrow.position.x < 0 )
@@ -73,7 +129,8 @@ function enemyAI()
     stage.removeChild(arrow);
   }
   arrow.position.x -= 10;
-}
+}*/
+
 
 function animate()
 {
@@ -82,7 +139,6 @@ function animate()
 }
 
 // Code from http://creativejs.com/resources/requestanimationframe/
-var fps = 60;
 function draw() {
     setTimeout(function() {
         requestAnimationFrame(draw);
